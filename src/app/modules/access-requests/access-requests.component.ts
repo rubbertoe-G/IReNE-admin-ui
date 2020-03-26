68535bb85bb75d0a323055deb0a87c5aa809b73b
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-03-26 03:02:02 -0400

    feat(access request): implemented dynamic row removal and alerts

--- src/app/modules/access-requests/access-requests.component.ts
+++ src/app/modules/access-requests/access-requests.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import Swal from 'sweetalert2';
 
 export interface RequestMeta {
   requestNbr: number;
@@ -43,6 +44,56 @@ export class AccessRequestsComponent implements OnInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  denyAccessRequest(request: RequestMeta) {
+    Swal.fire({
+      title: 'Deny Access Request',
+      text: 'Are you sure you want to deny this access request?',
+      icon: 'warning',
+      showCancelButton: true,
+      showConfirmButton: true,
+      showLoaderOnConfirm: true,
+      confirmButtonColor: 'red',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Denied!',
+          text: "The access request has been denied.",
+          showConfirmButton: false,
+          timer: 1500
+        })
+        let index = this.dataSource.data.indexOf(request)
+        this.dataSource.data.splice(index, 1);
+        this.dataSource._updateChangeSubscription();
+      }
+    });
+  }
+
+  acceptAccessRequest(request: RequestMeta){
+    Swal.fire({
+      title: 'Accept Access Request',
+      text: 'Are you sure you want to accept this access request?',
+      icon: 'warning',
+      showCancelButton: true,
+      showConfirmButton: true,
+      showLoaderOnConfirm: true,
+      confirmButtonColor: 'green',
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Accepted!',
+          text: "The access request has been accepted.",
+          showConfirmButton: false,
+          timer: 1500
+        })
+        let index = this.dataSource.data.indexOf(request)
+        this.dataSource.data.splice(index, 1);
+        this.dataSource._updateChangeSubscription();
+      }
+    });    
   }
 
 }
