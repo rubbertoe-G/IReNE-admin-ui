1682c9b11a49b5c69445d970e1c4755fdfac3ef4
Yomar Ruiz Santos <yomar.ruiz@upr.edu>
2020-04-19 17:19:51 -0400

    docs(login): added documentation for the login module

--- src/app/layouts/login/login.component.ts
+++ src/app/layouts/login/login.component.ts
@@ -11,19 +11,44 @@ import {
   FormControl
 } from "@angular/forms";
 
-
+/**
+ * Component that manages all the necessary procedures for a user to login.
+*/
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  /**
+   * Forms with the fields and validators that are going to be used in the login form.
+  */
   loginForm: FormGroup;
+
+  /**
+   * Variable that determines if the login is still waiting for the backend response.
+  */
   loading = false;
-  submitted = false;
+
+  /**
+   * Url that the user is going to go to after login in.
+  */
   returnUrl: string;
+
+  /**
+   * Flag variable that tells if there are any incorrect fields.
+  */
   incorrectFields = false;
 
+  /**
+   * Construct the Login component with a router, an authentication service, an activated route and a form builder.
+   * 
+   * @param {Router} router router object to be used
+   * @param {AuthenticationService} authenticationService authentication server for backend communication
+   * @param {ActivatedRoute} route route to access query params
+   * @param {FormBuilder} fb form builder for the login form
+   */
   constructor(
     private router: Router,
     private authenticationService: AuthenticationService,
@@ -35,6 +60,9 @@ export class LoginComponent implements OnInit {
       }
      }
 
+  /**
+   * Initialization function that gives the form builder all the validators needed for each field.
+   */
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       username: new FormControl('', [
@@ -50,15 +78,20 @@ export class LoginComponent implements OnInit {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/collaborators';
   }
 
-  // Run login if user pressed enter on form
+  /**
+   * 
+   * @ignore 
+   */
   keyDownLogin(e: KeyboardEvent){
     if(e.keyCode == 13){
       this.login()
     }
   }
 
+  /**
+   * Login function that performs the necessary procedures to try and acquire a token.
+   */
   login(){
-    this.submitted = true;
     this.loading = true;
     this.authenticationService.login(this.loginForm.value.username, this.loginForm.value.password)
         .pipe(first())
