ecf10fb29c4b6199d8e97a59c208978d69051ccf
Roberto Guzman <roberto.guzman3@upr.edu>
2020-05-07 11:16:50 -0400

    Error modal content

--- src/app/shared/components/modals/error-modal/error-modal.component.ts
+++ src/app/shared/components/modals/error-modal/error-modal.component.ts
@@ -1,16 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  FormControl
-} from "@angular/forms";
 
 export interface DialogData{
   title: string;
   message: string;
-  status: string;
 }
 
 @Component({
